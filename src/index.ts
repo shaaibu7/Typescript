dcabf08d1d0dcc83fda11ffc116835e6b0be0f8c
shaaibu <shaaibusuleiman@gamil.com>
2023-09-09 09:52:53 +0100

    union with different types

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,40 @@
 // Describing an interface for object shape
 
 interface User {
-    name: string;
-    id: number;
+  name: string;
+  id: number;
 }
 
 const user: User = {
-    name: "Shaaibu",
-    id: 12,
-}
+  name: "Shaaibu",
+  id: 12,
+};
 
 // interface declaration with classes
 
 class UserAccount {
-    name: string;
-    id: number;
+  name: string;
+  id: number;
 
-    constructor(name: string, id: number) {
-        this.name = name;
-        this.id = id
-    }
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+  }
 }
 
-const userone: User = new UserAccount('Murphy', 1)
+const userone: User = new UserAccount("Murphy", 1);
 
-console.log(userone.name)
+console.log(userone.name);
 
+// Unions
+type Mybool = true | false;
+
+let IsPositive: Mybool = true;
+
+// A popular use-case for union types is to describe 
+// the set of string or number literals that a value is
+//  allowed to be:
+
+type WindowStates = 'open' | 'closed' | 'minimal';
+type LockStates = 'locked' | 'unlocked';
+type PositiveOddNumberUnderTen = 1 | 3 |5 | 7 | 9;
